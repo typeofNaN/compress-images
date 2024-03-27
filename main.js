0371dd94d30dd672b6d9fe15318b8fbe72ff6afa
typeofNaN <dmdefine6@gmail.com>
2024-03-27 09:55:40 +0800

    perf: optimize 💯

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
-const images = require('images')
 const fs = require('fs')
+const images = require('images')
 
-const path = 'images'
+const inDirPath = 'images/'
+const outDirPath = 'compress/'
 
-function exp(path) {
+/**
+ * @description 压缩图片
+ * @param { string } path 目标图片文件夹路径
+ * @param { string } quality 压缩质量 0 - 100 默认30
+ */
+function compressImage(path, quality = 30) {
   fs.readdir(path, (err, files) => {
     if (err) {
-      console.log('error:\n' + err)
-      return
+      throw Error('error:\n' + err)
     }
 
-    files.forEach((file) => {
-      fs.stat(path + '/' + file, (err, stat) => {
-        if (err) {
-          console.log(err)
-          return
+    files.forEach(file => {
+      fs.stat(path + file, (error, stat) => {
+        if (error) {
+          throw Error(error)
         }
+
         // 如果是文件夹，则递归
         if (stat.isDirectory()) {
-          exp(path + '/' + file)
+          compressImage(path + file)
         } else {
           // 读出所有的图片文件
           if (file.indexOf('.png') >= 0 || file.indexOf('.jp') >= 0) {
-            console.log('文件名:' + path + '/' + file)
-            const name = path + '/' + file
-            const outName = 'compress/' + file
-
-            images(name)
-              .save(outName, {
-                quality: 30
-              })
+            const name = path + file
+            const outName = outDirPath + file
+            
+            console.log(`文件${name}正在压缩...`)
+            images(name).save(outName, { quality })
+            console.log(`文件${name}压缩成功！`)
           }
         }
       })
     })
   })
 }
-exp(path)
+
+compressImage(inDirPath)
